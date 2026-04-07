56f4a30843514825111feb7714bda6b6b9306b96
Dibyendu <khaskeldibyendu22@gmail.com>
2026-04-07 22:18:18 +0530

    Variable in php

---
+++ variable.php
@@ -0,0 +1,30 @@
+<?php
+
+//Php is as same all types of vatiable which is any tipicaly programming language have
+//and varible in php starts with a $
+
+$num=5;
+echo $num;
+
+$name="Dibyendu";
+
+echo $name;
+echo "<br/>";
+$bool=true;
+echo $bool;
+echo "<br/>";
+$flo=1.5;
+echo $flo;
+echo "<br/>";
+//php has a funtion to check ver type gettype()
+
+echo gettype($name);
+echo "<br/>";
+echo gettype($num);
+echo "<br/>";
+echo gettype($bool);
+echo "<br/>";
+echo gettype($flo);
+
+
+?>
