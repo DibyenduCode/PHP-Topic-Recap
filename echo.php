49fd06482ae67850c1e726749e6a5af64a8cc3b2
Dibyendu <khaskeldibyendu22@gmail.com>
2026-04-07 22:03:35 +0530

    add echo .php it show echo keyword with html and inline css

---
+++ echo.php
@@ -0,0 +1,13 @@
+<?php
+//Normal echo in php
+echo "Welcome to PHP";
+
+//echo with html
+echo "<h1>Welcome to PHP</h1>";
+
+//echo with inlline css
+
+echo "<h1 style='color:red;'>Welcome to PHP</h1>";
+
+
+?>
