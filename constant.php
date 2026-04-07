12354b5c5d37fbb6b92cb5adffce28e7499b1d8f
Dibyendu <khaskeldibyendu22@gmail.com>
2026-04-07 22:31:53 +0530

    constanat in php

---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+//constant is a special type of variable which you can not overwrite
+
+const a=10;
+echo a;
+
+// if you now try to reasign this variable you  "Constant a already defined"
+
+// const a=20;
+
+?>
